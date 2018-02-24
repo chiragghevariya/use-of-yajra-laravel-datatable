67194575798a14f0af9ad2ddd775d988d0adde80
chiragghevariya <gcb1196@gmail.com>
2018-02-25 01:25:51 +0530

    how to add new column

--- app/Http/Controllers/YajraController.php
+++ app/Http/Controllers/YajraController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use DB;
 use Yajra\Datatables\Datatables;
 
 
@@ -19,7 +20,18 @@ class YajraController extends Controller
 	public function getDataTable()
 	{
 
-	    return Datatables::of(User::all())->make(true);
+	     // /return Datatables::of(User::query())->make(true);
+
+	     $users = DB::table('users')
+            ->select(['id', 'name', 'email', 'password', 'created_at', 'updated_at']);
+
+        return Datatables::of($users)
+            ->addColumn('action', function ($us) {
+                return '<a href="'.url('edit/'.$us->id).'" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-edit"></i> Edit</a>'.'  '.'<a href="'.url('edit/'.$us->id).'" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-delete"></i> Delete</a>';
+            })
+			->editColumn('id', 'ID: {{$id}}')
+            ->removeColumn('password')
+            ->make(true);
 	}
 
 }
--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -9,6 +9,8 @@
                 <th>Email</th>
                 <th>Created At</th>
                 <th>Updated At</th>
+                <th>Action</th>
+                
             </tr>
         </thead>
     </table>
@@ -20,13 +22,14 @@ $(function() {
     $('#users-table').DataTable({
         processing: true,
         serverSide: true,
-        ajax: '{{ route('getDatatablename') }}',
+        ajax: '{!! route('getDatatablename') !!}',
         columns: [
             { data: 'id', name: 'id' },
             { data: 'name', name: 'name' },
             { data: 'email', name: 'email' },
             { data: 'created_at', name: 'created_at' },
-            { data: 'updated_at', name: 'updated_at' }
+            { data: 'updated_at', name: 'updated_at' },
+            { data: 'action', name: 'action' }
         ]
     });
 });
